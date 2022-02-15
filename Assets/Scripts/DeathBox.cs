67ec8a9fb26278823abd228837b2d6c041dbacab
Denison Percey <denisonipercey@outlook.com>
2022-02-15 10:58:01 -0400

    made death box prefab

--- Assets/Scripts/DeathBox.cs
+++ Assets/Scripts/DeathBox.cs
@@ -4,37 +4,28 @@ using UnityEngine;
 
 public class DeathBox : MonoBehaviour
 {
-    //public Vector3 playerPos;
-    //public Vector3 spawn;
-    //public Vector3 checkPoint;
-    // public GameObject checkPointPos;
-    //CharacterController cc;
+    public Vector3 spawn;
+    public Vector3 checkPoint;
+    public GameObject checkPointPos;
     public GameObject player;
     public GameObject spawnPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        //spawn = spawnPos.transform.position;
-        //checkPoint = spawnPos.transform.position;
-        //cc = GetComponent<CharacterController>();
-
+        spawn = spawnPos.transform.position;
+        checkPoint = spawnPos.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //playerPos = player.transform.position;
         
     }
     private void OnTriggerEnter(Collider other)
     {
-        //Debug.Log(player.transform.position);
-        //Debug.Log("collided");
-        //cc.enabled = false;
         player.GetComponent<CharacterController>().enabled = false;
-        player.transform.position = spawnPos.transform.position;
+        player.transform.position = spawn;
         player.GetComponent<CharacterController>().enabled = true;
-        //cc.enabled = true;
     }
 }
